1db5b5adc2d99d661ab7041923192f7e34e4b7b0
Saurabh <pandey.g.saurabh97@gmail.com>
2022-06-04 22:36:29 +0530

    created stocks migration and added relevent relations

--- database/migrations/2022_06_04_162232_create_stocks_table.php
+++ database/migrations/2022_06_04_162232_create_stocks_table.php
@@ -15,6 +15,40 @@ class CreateStocksTable extends Migration
     {
         Schema::create('stocks', function (Blueprint $table) {
             $table->id();
+            $table->string('roll_no')->unique();
+            $table->string('grade');
+            $table->double('meter');
+            $table->double('width');
+            $table->double('weight');
+
+            // one to many relation with purchases table
+            $table->unsignedBigInteger('purchases_id');
+            $table->foreign('purchases_id')
+                    ->references('id')
+                    ->on('purchases')
+                    ->onDelete('cascade');
+
+            // one to many relation with client_id on purchases
+            $table->unsignedBigInteger('purchase_client_id');
+            $table->foreign('purchase_client_id')
+                    ->references('client_id')
+                    ->on('purchases')
+                    ->onDelete('cascade');
+
+            // one to many relation with item_id on purchases
+            $table->unsignedBigInteger('purchase_item_id');
+                    $table->foreign('purchase_item_id')
+                            ->references('item_id')
+                            ->on('purchases')
+                            ->onDelete('cascade');
+                            
+            // one to many relation with transport_id on purchase
+            $table->unsignedBigInteger('purchase_transport_id');
+            $table->foreign('purchase_transport_id')
+                    ->references('transport_id')
+                    ->on('purchases')
+                    ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
